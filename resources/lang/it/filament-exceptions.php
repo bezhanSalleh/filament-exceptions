017d1b42021a8d03eeb7f951e9a03cdac3259eb1
Francesco Apruzzese <inerba@gmail.com>
2026-03-24 17:18:46 +0100

    Add Italian language support for filament exceptions

---
+++ resources/lang/it/filament-exceptions.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'labels' => [
+        'model' => 'Eccezione',
+        'model_plural' => 'Eccezioni',
+        'navigation' => 'Eccezione',
+        'navigation_group' => 'Impostazioni',
+    ],
+
+    'empty_list' => 'Evviva! Rilassati e goditela 😎',
+
+    'columns' => [
+        'method' => 'Metodo',
+        'path' => 'Percorso',
+        'type' => 'Tipo',
+        'code' => 'Codice',
+        'ip' => 'IP',
+        'occurred_at' => 'Avvenuto il',
+    ],
+
+];
